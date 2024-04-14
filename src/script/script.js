250d0524be614c6962bab349dc64fb46a8f1d30d
Eduardo Secco <dudssecco.code@gmail.com>
2024-04-14 20:54:15 -0300

    Update

--- src/script/script.js
+++ src/script/script.js
@@ -1,1 +1,16 @@
-  
+const start = document.querySelector('.start')
+const end = document.querySelector('.end')
+const submit = document.querySelector('.enviar')
+const valor = document.querySelector('#valor')
+const valores = document.querySelectorAll('.btn')
+
+submit.addEventListener("click", () =>{
+    end.classList.remove('hidden')
+    start.style.display = 'none'
+})
+
+valores.forEach((rate) => {
+    rate.addEventListener("click", () => {
+        valor.innerHTML = rate.innerHTML
+    })
+})
